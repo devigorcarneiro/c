b45a1b329d003700a57d5dc0c7e0edc356ab0b86
Igor Carneiro <borgescapital@gmail.com>
2026-06-25 01:27:36 -0300

    feat: aula 1 - comentários em C

---
+++ 01-introducao-linguagem-c/comentarios-c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  // Isto é uma linha de comentário
+  /*
+   * Este é
+   * um comentário
+   * de várias linhas
+   */
+  printf("Hello, world!\n");
+  return 0;
+}
