4c286c7fb03d9c31adfa1dac235aa899eb117b81
Igor Carneiro <borgescapital@gmail.com>
2026-06-26 00:35:05 -0300

    feat: aula 3 - caracteres de escape

---
+++ 01-introducao-linguagem-c/caracteres-escape.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  /*
+   * Caracteres de escape
+   * \n -> pula uma linha
+   * \t -> tabulação
+   * \" -> para exibir aspas em strings
+   */
+  printf("Olá, mundo!\n");
+  return 0;
+}
