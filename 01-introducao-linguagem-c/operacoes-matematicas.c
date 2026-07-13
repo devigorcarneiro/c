2d811ddee52d96d0eb34624a39bb0b96635ad77f
Igor Carneiro <borgescapital@gmail.com>
2026-07-13 20:33:29 -0300

    feat: aula 4 - operações matemáticas

---
+++ 01-introducao-linguagem-c/operacoes-matematicas.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  // Operações matmáticas
+  printf("%d + %d = %d\n", 10, 15, 10 + 15);
+  printf("%d - %d = %d\n", 10, 15, 15 - 10);
+  printf("%d * %d = %d\n", 10, 15, 10 * 15);
+  printf("%d / %d = %d\n", 10, 15, 10 / 15);
+  return 0;
+}
